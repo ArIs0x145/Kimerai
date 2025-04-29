ba1df16b4804672d0d5d6d893c1b5872ec7603a9
ArIs0x145 <aris0x145@gmail.com>
2025-04-29 22:51:59 +0800

    chore: Initialize project structure, create folder hierarchy

---
+++ app/src/main/java/io/github/aris0x145/kimerai/KimeraiApplication.kt
@@ -0,0 +1,11 @@
+package io.github.aris0x145.kimerai
+
+import android.app.Application
+
+// TODO: Add Hilt annotation @HiltAndroidApp later
+class KimeraiApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialization code here if needed
+    }
+}
