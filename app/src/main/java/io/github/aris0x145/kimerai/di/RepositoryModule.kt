1bd49ae5c2af2d8233c71400a37728f385d35a59
ArIs0x145 <aris0x145@gmail.com>
2025-05-03 04:32:26 +0800

    feat: Add model ui

---
+++ app/src/main/java/io/github/aris0x145/kimerai/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package io.github.aris0x145.kimerai.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.aris0x145.kimerai.data.repository.ModelRepositoryImpl
+import io.github.aris0x145.kimerai.domain.repository.ModelRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    
+    @Binds
+    @Singleton
+    abstract fun bindModelRepository(
+        modelRepositoryImpl: ModelRepositoryImpl
+    ): ModelRepository
+}
---
+++ app/src/main/java/io/github/aris0x145/kimerai/ui/navigation/NavRoutes.kt
@@ -0,0 +1,11 @@
+package io.github.aris0x145.kimerai.ui.navigation
+
+/**
+ * 定義應用的主要導航路由
+ */
+object NavRoutes {
+    const val CHAT = "chat"
+    const val HISTORY = "history"
+    const val TOOLS = "tools"
+    const val MODEL_CONFIG = "model_config"
+}
